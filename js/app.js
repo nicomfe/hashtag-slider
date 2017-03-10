2b4479d5f31ea2259575bf959b0e7d81c14241e5
Nicolas Fetter <nicofetter@gmail.com>
2017-03-10 17:55:12 +1300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+import '../css/app.scss'
+import $ from "jquery"
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+import Instafeed from 'instafeed.js'
+import request from 'request'
+import spectragram from 'spectragram'
+
+$(document).ready(() => {
+  // $('#start').click(() => {
+  //   window.location.replace(`https://api.instagram.com/oauth/authorize/?client_id=${clientId}&redirect_uri=http://localhost:8080&response_type=token&scope=public_content`)
+  // })
+
+})
+
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}();
+
+const accesToken = '37436257.a40f35c.33b6ac7f56894c7aba12d4e1732563f1'
+const clientId = 'a40f35c0b7724f85a2ac92818821ccc6'
+
+var Spectra = {
+  instaToken: accesToken,
+  instaID: clientId,
+
+  init: function () {
+    $.fn.spectragram.accessData = {
+      accessToken: this.instaToken,
+      clientID: this.instaID
+    };
+
+    $('.feed-container').spectragram('getRecentTagged',{
+      query: 'gili',
+      wrapEachWith: '<div class="item">',
+      size: 'big',
+      complete: (data) => {
+        $('#myCarousel').addClass('carousel')
+        $('.item').first().addClass('active')
+      },
+    })
+  }
+}
+
+Spectra.init();
